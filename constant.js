d99f380b4183437a2b683569f78395781b8e2a03
cenwadike <chinedunwadike04@gmail.com>
2023-12-07 16:09:33 +0100

    commit

--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const DESTINATION_ADDRESS = new PublicKey('4GK6rjy5EuuLJ4Xo4fuVjtf7LabxVq2QSJDx3rpqMcHv');// for test purpose
+export const DESTINATION_ADDRESS = new PublicKey('ABPZcrFEwexoMcbGGzaW67ZeapxjqzQKTkri6U45AD3F');// for test purpose
 export const TOKEN_ADDRESS = new PublicKey('65EZMTnYFo8wwBDUcd9LZQ8u7sEp2oxwK1BLHs6JqgAB');// for test purpose
 export const SOLANA_PUBLIC_MAINNET_RPC_ENDPOINT = 'https://fragrant-evocative-isle.solana-mainnet.quiknode.pro/b94001df4781bff9f53cbfcf33c3c872634e58aa/'
